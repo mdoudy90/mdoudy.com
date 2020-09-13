dc0762ce3cd431661e1608a5df051439c067e511
Michael Doudy <michael.doudy@gmail.com>
2020-09-12 22:19:32 -0400

    Heroku prep

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
-const webpack = require('webpack');
 const path = require('path');
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
-const config = {
+module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
@@ -12,9 +11,13 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
+        test: /\.jsx?$/,
+        include: SRC_DIR,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/react']
+        }
       },
       {
         test: /\.scss$/,
@@ -23,30 +26,7 @@ const config = {
           'css-loader',
           'sass-loader'
         ]
-      },
-      {
-        test: /\.png$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              mimetype: 'image/png',
-            }
-          }
-        ]
-      },
-      {
-        test: /\.svg$/,
-        use: 'file-loader'
       }
     ]
-  },
-  resolve: {
-    extensions: [
-      '.js',
-      '.jsx'
-    ]
   }
-};
-
-module.exports = config;
+}
